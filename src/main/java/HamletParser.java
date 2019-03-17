631c1fb37818894721b8baa627cdf5cd23919bdf
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-03-17 13:04:12 -0400

    implemented and tested

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -9,31 +11,74 @@ public class HamletParser {
 
     private String hamletData;
 
+    private final boolean isCaseInSensitive = true;
+
     public HamletParser(){
         this.hamletData = loadFile();
+    }
+
+    public HamletParser(String inputString){
+        this.hamletData = inputString;
     }
 
     private String loadFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
-
         try(Scanner scanner = new Scanner(file)){
             while(scanner.hasNextLine()){
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
-
             scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
-
         return result.toString();
+    }
+
+    public void replaceAll(String toReplace, String newString){
+        Matcher matcher = getMatcher(toReplace);
+        //hamletData = matcher.replaceAll( newString );
+       hamletData = matcher.replaceAll( "$1" + newString + "$3" );
+
+    }
+
+    public boolean find(String toFind){
+        Matcher matcher = getMatcher(toFind);
+        boolean isFound = matcher.find();
+        return isFound;
+    }
+
+    private Matcher getMatcher(String toMatch){
+
+        Pattern pattern = null;
+
+        //String toMatchPattern = "\\b("+ toMatch + ")\\b"; // using word boundary
+        //String toMatchPattern = "(?m)(^|\\W+?)("+ toMatch + ")(\\W+?|$)";
+
+        String toMatchPattern = "(?m)(^|\\W{1})("+ toMatch + ")(\\W{1}|$)";
+
+        if(isCaseInSensitive)
+            pattern = Pattern.compile(toMatchPattern, Pattern.CASE_INSENSITIVE);
+        else
+            pattern = Pattern.compile(toMatchPattern);
+
+        Matcher matcher = pattern.matcher(hamletData);
+
+        return matcher;
+    }
+
+    public int getNumberOfMatches(String toFind){
+        Matcher matcher = getMatcher(toFind);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
     }
 
     public String getHamletData(){
         return hamletData;
     }
-
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,20 +13,119 @@ public class HamletParserTest {
         this.hamletParser = new HamletParser();
         this.hamletText = hamletParser.getHamletData();
     }
+    @Test
+    public void testChangeHamletToLeonFromFile() {
+        HamletParser parser = new HamletParser();
+        int beforeNumberofHamlet = parser.getNumberOfMatches("hamlet");
+        int beforeNumberofLeon = parser.getNumberOfMatches("leon");
+        parser.replaceAll("hamlet", "Leon");
+        int afterNumberofHamlet = parser.getNumberOfMatches("hamlet");
+        int afterNumberofLeon = parser.getNumberOfMatches("Leon");
+
+        Assert.assertEquals(afterNumberofHamlet, 0);
+        Assert.assertEquals(afterNumberofLeon, beforeNumberofHamlet + beforeNumberofLeon);
+    }
+
+
+    @Test
+    public void testChangeHoratioToTariqFromFile() {
+        String expected = "Tariq something else";
+        HamletParser parser = new HamletParser();
+        int beforeNumberofHoratio = parser.getNumberOfMatches("horatio");
+        int beforeNumberofTariq = parser.getNumberOfMatches("tariq");
+        parser.replaceAll("horatio", "Tariq");
+        int afterNumberofHoratio = parser.getNumberOfMatches("horatio");
+        int afterNumberofTariq = parser.getNumberOfMatches("Tariq");
+
+        Assert.assertEquals(afterNumberofHoratio, 0);
+        Assert.assertEquals(afterNumberofTariq, beforeNumberofHoratio + beforeNumberofTariq);
+    }
 
     @Test
     public void testChangeHamletToLeon() {
+        String expected = "Leon something else";
+        HamletParser parser = new HamletParser("Hamlet something else");
+        parser.replaceAll("hamlet", "Leon");
+        Assert.assertEquals(parser.getHamletData(), expected);
+
+    }
+    @Test
+    public void testChangeHamletToLeonSubsequentMultiple() {
+        String expected = "Leon  ,\nLeon\n else";
+        HamletParser parser = new HamletParser("Hamlet  ,\nhamlet\n else");
+        int num = parser.getNumberOfMatches("hamlet");
+        parser.replaceAll("hamlet", "Leon");
+        Assert.assertEquals(parser.getHamletData(), expected);
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String expected = "Tariq something else";
+        HamletParser parser = new HamletParser("Horatio something else");
+        parser.replaceAll("horatio", "Tariq");
+        Assert.assertEquals(parser.getHamletData(), expected);
     }
-
+    @Test
+    public void testChangeHoratioToTariqMultiple() {
+        String expected = "Tariq something Tariq else";
+        HamletParser parser = new HamletParser("Horatio something horatio else");
+        parser.replaceAll("horatio", "Tariq");
+        Assert.assertEquals(parser.getHamletData(), expected);
+    }
+    @Test
+    public void testFindHoratioSimple() {
+        HamletParser parser = new HamletParser("Horatio");
+        boolean retrievedFound = parser.find("horatio");
+        Assert.assertTrue(retrievedFound);
+    }
+    @Test
+    public void testReplaceHoratioFullCaps() {
+        HamletParser parser = new HamletParser("HORATIO jhsgdjhsgdjhsg");
+        parser.replaceAll("HORATIO", "LEON");
+        Assert.assertEquals(parser.getHamletData(), "LEON jhsgdjhsgdjhsg");
+    }
     @Test
     public void testFindHoratio() {
+        HamletParser parser = new HamletParser(" Horatio.");
+        boolean retrievedFound = parser.find("horatio");
+        Assert.assertTrue(retrievedFound);
     }
-
+    @Test
+    public void testFindHoratioMiddleFalse() {
+        HamletParser parser = new HamletParser(" fHoratiof.");
+        boolean retrievedFound = parser.find("horatio");
+        Assert.assertFalse(retrievedFound);
+    }
+    @Test
+    public void testFindHoratioFalse() {
+        HamletParser parser = new HamletParser(" fHoyytof.");
+        boolean retrievedFound = parser.find("horatio");
+        Assert.assertFalse(retrievedFound);
+    }
+    @Test
+    public void testFindHamletSimple() {
+        HamletParser parser = new HamletParser("Hamlet");
+        boolean retrievedFound = parser.find("hamlet");
+        Assert.assertTrue(retrievedFound);
+    }
     @Test
     public void testFindHamlet() {
+        HamletParser parser = new HamletParser(" Hamlet.");
+        boolean retrievedFound = parser.find("haMlet");
+        Assert.assertTrue(retrievedFound);
     }
+    @Test
+    public void testFindHamletMiddleFalse() {
+        HamletParser parser = new HamletParser(" fHamletf.");
+        boolean retrievedFound = parser.find("hamlet");
+        Assert.assertFalse(retrievedFound);
+    }
+    @Test
+    public void testFindHamletFalse() {
+        HamletParser parser = new HamletParser(" fHoyytof.");
+        boolean retrievedFound = parser.find("horatio");
+        Assert.assertFalse(retrievedFound);
+    }
+
 }
